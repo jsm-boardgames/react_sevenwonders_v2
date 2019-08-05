df54b33153c946dad8ef81f78ab6bc7bde808bfd
Jacob McCrumb <jsmccrumb@gmail.com>
2019-08-04 23:31:57 -0400

    switch tailwind to own file and use some classes for cards

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 function App() {
   const svgWidth = 1000;
   const maxCards = 7;
+  const cardClasses = ['sw-military-card', 'sw-cultural-card', 'sw-natural-resource-card'];
   const viewBox = `0 0 ${svgWidth} ${svgWidth}`;
   return (
     <div className='App'>
@@ -16,11 +17,12 @@ function App() {
         {[0,1,2,3,4,5, 6].map((idx, _, arr) => {
           const midway = (arr.length - 1) / 2;
           const rotate = `rotate(${10 * (idx - midway)} 575 200)`;
+          const cn = `sw-card ${cardClasses[idx % 3]}`;
           return (
           <React.Fragment key={idx}>
-            <g transform={rotate}>
-              <rect x={500 + (60 * (idx - midway))} y='30' width='150' height='220' fill='blue' stroke='red' stroke-width='1'/>
-              <text x={505 + (60 * (idx - midway))} y='45' textLength="90" class='text-sm'>I'm #{idx}</text>
+            <g className={cn} transform={rotate}>
+              <rect x={500 + (60 * (idx - midway))} y='30' width='150' height='220' />
+              <text x={505 + (60 * (idx - midway))} y='45' textLength="90" className='text-sm'>I'm #{idx}</text>
             </g>
           </React.Fragment>
         )
