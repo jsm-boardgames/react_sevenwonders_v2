3a3a7c27aa6e4925d3a5321122b79b847e391c76
Jacob McCrumb <jsmccrumb@gmail.com>
2019-11-11 11:05:10 -0500

    Add stage to playerInfo div, resolves #1

--- src/components/PlayersInfo.js
+++ src/components/PlayersInfo.js
@@ -2,6 +2,11 @@ import React from 'react';
 import GameBadge from './GameBadge';
 
 const PlayerInfo = ({playerName = '', coins = 0, military = 0, stagesInfo = [], wonderSide = 'a', wonderName = '', wonderResource = '', cardsPlayed = []}) => {
+  const stage = stagesInfo.filter(s => s.isBuilt).length;
+  const stageNum = stage === 4 ? 'IV' : 'I'.repeat(stage);
+  const wonderDisplay = stage === 0 ?
+      `${wonderName} side: ${wonderSide}` :
+      `${wonderName} side: ${wonderSide} stage: ${stageNum}`;
   const badges = Object.entries(cardsPlayed.reduce((acc, curr) => {
     acc[curr.type] = !acc[curr.type] ? 1 : acc[curr.type] + 1;
     return acc;
@@ -11,7 +16,7 @@ const PlayerInfo = ({playerName = '', coins = 0, military = 0, stagesInfo = [], 
     <div className='w-full flex flex-col bg-blue-200 m-2 p-4 rounded-lg'>
       <div>{playerName}</div>
       <div><GameBadge type='treasury' value={formattedCoins} /> {badges}</div>
-      <div>{wonderName} - {wonderSide}</div>
+      <div>{wonderDisplay}</div>
     </div>
   );
 };
